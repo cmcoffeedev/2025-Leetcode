bd638d8a3ff75986f6f0cb45650bb1bea1a39422
Christopher Coffee <chrisc1891@gmail.com>
2025-02-12 09:14:53 -0800

    Time: 266 ms (35.8%), Space: 33.9 MB (58.02%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.kt
+++ 0278-first-bad-version/0278-first-bad-version.kt
@@ -3,10 +3,11 @@
 
 class Solution: VersionControl() {
     override fun firstBadVersion(n: Int) : Int {
-        var left = 1
+
+        var left = 0
         var right = n
         while(left < right){
-            val mid = left + (right - left)/2
+            val mid = left + (right-left)/2
             if(isBadVersion(mid)){
                 right = mid
             }
@@ -14,7 +15,7 @@ class Solution: VersionControl() {
                 left = mid + 1
             }
         }
-        
         return left
+        
 	}
 }
