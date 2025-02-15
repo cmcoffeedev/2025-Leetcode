bf225755b46df017bed7624f8dde42cf5eee2d72
Christopher Coffee <chrisc1891@gmail.com>
2025-02-15 01:50:26 -0800

    Time: 16 ms (14.05%), Space: 41.4 MB (10.99%) - LeetHub

--- 0704-binary-search/0704-binary-search.kt
+++ 0704-binary-search/0704-binary-search.kt
@@ -6,11 +6,11 @@ class Solution {
             val mid = left + (right - left)/2
             val midVal = nums[mid]
             if(target == midVal) return mid
-            if(target > midVal){
-                left = mid + 1
+            if(target < midVal){
+                right = mid - 1
             }
             else{
-                right = mid - 1
+                left = mid + 1
             }
         }
         return -1
