c28079ee4c4657ded1fc38edad1cb51edbb00d51
Christopher Coffee <chrisc1891@gmail.com>
2025-02-17 15:52:57 -0800

    Time: 37 ms (18.72%), Space: 40.1 MB (12.79%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.kt
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.kt
@@ -1,20 +1,18 @@
 class Solution {
     fun lengthOfLongestSubstring(s: String): Int {
-        
         var longest = 0
         var left = 0
-        
-        val set = HashSet<Char>()
-        
-        for(right in 0 until s.length){
-            var current = s[right]
-            while(set.contains(current) && left < right){
-                set.remove(s[left++])
+        val set = ArrayDeque<Char>()
+
+        s.forEachIndexed { right, char ->
+            while(set.contains(char) && left < right){
+                set.remove(s[left])
+                left++
             }
-            set.add(current)
+            set.add(char)
             longest = Math.max(longest, (right - left) + 1)
-            
         }
+
         return longest
     }
 }
