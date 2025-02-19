b781b85bbe554347cc9d6dfa547768d78486db2c
Christopher Coffee <chrisc1891@gmail.com>
2025-02-18 22:18:48 -0800

    Time: 283 ms (31.95%), Space: 134.7 MB (94.44%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.kt
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.kt
@@ -4,18 +4,10 @@ class Solution {
         if(source == destination) return true
 
         val adjList = mutableMapOf<Int, MutableList<Int>>()
-        for(row in 0 until edges.size){
-            val first = edges[row][0]
-            val second = edges[row][1]
-
-            val listOne = adjList.getOrDefault(first, mutableListOf<Int>())
-            val listTwo = adjList.getOrDefault(second, mutableListOf<Int>())
-
-            listOne.add(second)
-            listTwo.add(first)
-
-            adjList[first] = listOne
-            adjList[second] = listTwo
+        
+        edges.forEach{ (first, second) ->
+            adjList.computeIfAbsent(first) { mutableListOf() }.add(second)
+            adjList.computeIfAbsent(second) { mutableListOf() }.add(first)
         }
 
         val seen = mutableSetOf<Int>() 
@@ -26,12 +18,11 @@ class Solution {
         while(stack.isNotEmpty()){
             val node = stack.removeLast()
             if(node == destination) return true
-            adjList[node]?.let{
-                it.forEach{ destItem ->
-                    if(!seen.contains(destItem)){
-                        seen.add(destItem)
-                        stack.addLast(destItem)
-                    }
+            
+            adjList[node]?.forEach { neighbor ->
+                if(!seen.contains(neighbor)){
+                    seen.add(neighbor)
+                    stack.addLast(neighbor)
                 }
             }
         }
