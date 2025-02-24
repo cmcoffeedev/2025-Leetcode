8bfcfe588151b75c0c6485435e873f7d86b4ed62
Christopher Coffee <chrisc1891@gmail.com>
2025-02-23 22:40:30 -0800

    Time: 23 ms (48.52%), Space: 38.1 MB (73%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        
+        if(s.length != t.length) return false
+        
+        val map = mutableMapOf<Char,Int>()
+        s.forEach{ char ->
+            map[char] = map.getOrDefault(char, 0) + 1
+        } 
+        
+        t.forEach{ char ->
+            val count = map[char] ?: return false
+            if(count == 0) return false
+            map[char] = count-1
+        }
+        
+        return true
+    }
+}
