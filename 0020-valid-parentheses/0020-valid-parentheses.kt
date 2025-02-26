f76ea2bc08cc436803a5a33b1c951a73094c0a31
Christopher Coffee <chrisc1891@gmail.com>
2025-02-25 23:46:21 -0800

    Time: 24 ms (24.31%), Space: 39.3 MB (17.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.kt
+++ 0020-valid-parentheses/0020-valid-parentheses.kt
@@ -1,23 +1,18 @@
 class Solution {
     fun isValid(s: String): Boolean {
-        val map = mapOf<Char,Char>(
-            ')' to '(',
+        val map = mapOf<Char, Char>(
             '}' to '{',
+            ')' to '(',
             ']' to '[',
         )
 
         val stack = ArrayDeque<Char>()
 
         s.forEach{ char ->
-            if(map.containsKey(char)){
-                if(stack.isEmpty() || map[char] != stack.removeFirst()) return false
-            }
-            else{
-                stack.addFirst(char)
-            }
+            map[char]?.let { removedChar ->
+                if(stack.isEmpty() || stack.removeLast() != removedChar) return false
+            } ?: stack.addLast(char)
         }
-
-        return if( stack.isEmpty() ) true else false
-        
+        return if(stack.isEmpty()) true else false
     }
 }
