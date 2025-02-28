b3bc5d56bc8920245652a41d0c744b6334c5afc1
Christopher Coffee <chrisc1891@gmail.com>
2025-02-28 00:17:27 -0800

    Time: 0 ms (100%), Space: 37.7 MB (54.96%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.kt
+++ 0206-reverse-linked-list/0206-reverse-linked-list.kt
@@ -9,18 +9,14 @@
  */
 class Solution {
     fun reverseList(head: ListNode?): ListNode? {
-        
         var tmp = head
-        var prev:ListNode? = null 
-        
+        var prev: ListNode? = null
         while(tmp != null){
-            val next = tmp.next
-            tmp.next = prev
+            val next = tmp?.next
+            tmp?.next = prev
             prev = tmp
             tmp = next
         }
-        
         return prev
-        
     }
 }
