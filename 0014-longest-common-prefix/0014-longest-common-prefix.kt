1cd1e6a33b5afa2d8e9d1118d12e7164a299d6ba
Christopher Coffee <chrisc1891@gmail.com>
2025-03-03 00:57:17 -0800

    Time: 1 ms (98.84%), Space: 36.1 MB (79.46%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.kt
+++ 0014-longest-common-prefix/0014-longest-common-prefix.kt
@@ -1,19 +1,21 @@
 class Solution {
     fun longestCommonPrefix(strs: Array<String>): String {
-        val map = mutableMapOf<String, Int>()
+        var minLen = Int.MAX_VALUE
 
-        var lcp = ""
-        strs.forEach{ item ->
-            val strBuilder = StringBuilder()
-            item.forEach{ char ->
-                strBuilder.append(char)
-                val str = strBuilder.toString()
-                map[str] = map.getOrDefault(str, 0) + 1
-                var size = map[str] ?: 0
-                if(size >= strs.size && str.length > lcp.length) lcp = str 
-            }
+        for(s in strs){
+            minLen = minOf(minLen, s.length)
         }
 
-        return lcp
+        var i = 0
+        while(i < minLen){
+            for(s in strs){
+                if(s[i] != strs[0][i]){
+                    return s.substring(0, i)
+                }
+            }
+            i++
+        }
+
+        return strs[0].substring(0, i)
     }
 }
