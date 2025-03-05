ae4ee04ccd76877a4c2d226b347653b6fc905076
Christopher Coffee <chrisc1891@gmail.com>
2025-03-05 12:22:07 -0800

    Time: 27 ms (16.69%), Space: 40.8 MB (9.03%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.kt
+++ 0125-valid-palindrome/0125-valid-palindrome.kt
@@ -1,19 +1,19 @@
 class Solution {
     fun isPalindrome(s: String): Boolean {
-        var start = 0
-        var end = s.length-1
 
-        while(start < end){
-            if( !s[start].isLetterOrDigit() ){
-                start++
-                continue
-            } 
-            if( !s[end].isLetterOrDigit() ){
-                end--
-                continue
-            } 
-            if(!s[start++].equals(s[end--], ignoreCase = true)) return false
+        val stack = ArrayDeque<Char>()
+
+        for(char in s){
+            if(!char.isLetterOrDigit()) continue
+            stack.addLast(char.toLowerCase())
         }
+
+        for(i in 0 until s.length){
+            var current = s[i].toLowerCase()
+            if(!current.isLetterOrDigit()) continue
+            if(stack.removeLast() != current) return false
+        }
+        if(stack.isNotEmpty()) return false
 
         return true
     }
