12dfbf3d77282dea425e2e96326abebacac15e29
Christopher Coffee <chrisc1891@gmail.com>
2025-04-18 03:24:03 -0400

    Time: 10 ms (77.36%), Space: 45.1 MB (52.07%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii-04-18-2025-00-24-02.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
+        val (shorter,longer) = if(nums1.size > nums2.size){
+            nums2 to nums1
+        }
+        else{
+            nums1 to nums2
+        }
+        
+        val map = mutableMapOf<Int, Int>()
+        
+        shorter.forEach{
+            map[it] = map.getOrDefault(it, 0) + 1
+        }
+        
+        val list = mutableListOf<Int>()
+        
+        longer.forEach{
+            var count = map[it] ?: return@forEach
+            if(count > 0){
+                list.add(it)
+                map[it] = count - 1
+            }
+        }
+        
+        return list.toIntArray()
+    }
+}
