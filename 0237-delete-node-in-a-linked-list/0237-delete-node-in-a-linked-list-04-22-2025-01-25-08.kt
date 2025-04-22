4d351fca2499af22217aa9ae2d230f029a46406a
Christopher Coffee <chrisc1891@gmail.com>
2025-04-22 04:25:09 -0400

    Time: 184 ms (8.57%), Space: 36.9 MB (99.05%) - LeetHub

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list-04-22-2025-01-25-08.kt
@@ -0,0 +1,19 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun deleteNode(node: ListNode?) {
+        val nextVal = node?.next?.`val`
+        val nextNode = node?.next?.next
+        node?.`val` = nextVal
+        node?.next = nextNode
+        
+    }
+}
